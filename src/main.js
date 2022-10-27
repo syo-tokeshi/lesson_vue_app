55ab7aa135b24edb539b4f3d55ece25773984d38
syo tokeshi <tokeshi@mfgnt.com>
2022-10-27 18:53:03 +0900

    css追加 asset/css/styles.cssに記述する

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import './assets/css/styles.css' // ★追記
+
 
 Vue.config.productionTip = false
 
