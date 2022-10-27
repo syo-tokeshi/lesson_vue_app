21a78b4c4b0233b61384ed2684b1996adfa0828c
syo tokeshi <tokeshi@mfgnt.com>
2022-10-27 19:02:04 +0900

    Board画面に、リストを追加するコンポーネントを作成

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <h1>test</h1>
+    <ListAdd></ListAdd>
   </div>
 
 </template>
 
 <script>
+import ListAdd from "@/components/ListAdd";
 export default {
-
+  components: {
+    ListAdd,
+  },
 }
 </script>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,29 @@
+<template>
+  　<!-- ★ここから追記 -->
+  <form class="addlist" @submit.prevent="addList">
+    <input v-model="title"
+           type="text"
+           class="text-input"
+           placeholder="Add new list"
+    >
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+  <!-- ★ここまで追記 -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ""
+    }
+  },
+  methods: {
+    addList() {
+      this.$store.dispatch("addlist",{title: this.title})
+    }
+  },
+}
+</script>
