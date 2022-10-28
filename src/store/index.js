ae09ccb113af0c28cef3befa607a2c2e4fd1a5fb
syo tokeshi <tokeshi@mfgnt.com>
2022-10-28 12:42:13 +0900

    localstrageの基本。普通はバックエンドかfirebaseを用意する

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,29 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const savedLists = localStorage.getItem("trello-lists")
+
+const store = new Vuex.Store({
   state: {
-    lists:[]
+    lists: savedLists ? JSON.parse(savedLists):[
+      {
+        title: "neko",
+        cards: [
+          {body: "wanko"},
+          {body: "salu"},
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [
+          { body: 'Science' }
+        ]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ]
   },
   getters: {
   },
@@ -22,3 +42,10 @@ export default new Vuex.Store({
   modules: {
   }
 })
+
+// mutationが呼ばれたら実行される
+store.subscribe((mutation,state) => {
+  localStorage.setItem("trello-lists",JSON.stringify(state.lists))
+})
+
+export default store
